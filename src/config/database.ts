ddc74eec686ac9d075204e1d2fc9147405a959c2
Élcio Mateus Fernandes <elciomateusfernandes1@gmail.com>
2025-07-22 09:39:27 -0300

    feat: implement graceful shutdown for Prisma client

--- src/config/database.ts
+++ src/config/database.ts
@@ -16,6 +16,17 @@ export const prisma =
 
 if (config.node_env !== "production") globalForPrisma.prisma = prisma;
 
+// Graceful shutdown
 process.on("beforeExit", async () => {
   await prisma.$disconnect();
+});
+
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
+
+process.on("SIGTERM", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
 });
